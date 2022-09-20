20b6bf875e139c1a6bd7e0467d4ec25907d07dcd
Ash Walker <ashurstwalker@gmail.com>
2022-09-20 15:46:58 -0400

    fix: get rid of the xremap stuff

--- src/keyboard.nix
+++ src/keyboard.nix
@@ -10,22 +10,5 @@ in {
   options = with lib; {};
   imports = [];
   config = {
-    services.xremap = {
-      enable = lib.mkDefault true;
-      package = pkgs.xremap;
-      services = {
-        "primary" = {
-          watch.configFile = false;
-          watch.devices = true;
-          watch.ignore = [];
-          devices = [];
-          settings = {
-            modmap = [];
-            keymap = [];
-          };
-        };
-      };
-      # configurations = mapAttrs (name: service: service.config) config.services.xremap.services;
-    };
   };
 }
