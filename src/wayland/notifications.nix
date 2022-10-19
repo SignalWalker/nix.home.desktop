9e3498be27d74f27566f7c9a9577af404a6f663f
Ash Walker <ashurstwalker@gmail.com>
2022-10-19 15:08:47 -0400

    fix(mako): erroneous import

--- src/wayland/notifications.nix
+++ src/wayland/notifications.nix
@@ -12,22 +12,5 @@ in
     enable = (mkEnableOption "Notification daemon") // { default = true; };
   };
   imports = lib.signal.fs.path.listFilePaths ./notifications;
-  config = lib.mkIf (cfg.enable && cfg.notifications.enable) {
-    programs.mako = {
-      enable = cfg.notifications.enable;
-      extraConfig = readFile ./mako/config;
-    };
-    systemd.user.services."mako" = {
-      Unit = {
-        Description = "Mako notification daemon";
-        Documentation = "man:mako(1)";
-        PartOf = [ "wayland-session.target" ];
-      };
-      Service = {
-        Type = "dbus";
-        BusName = "org.freedesktop.Notifications";
-        ExecStart = "mako";
-      };
-    };
-  };
+  config = lib.mkIf (cfg.enable && cfg.notifications.enable) { };
 }
