2c42148d98544b2e1463416fb616fbbcb8527733
Ash Walker <ashurstwalker@gmail.com>
2022-10-19 15:19:42 -0400

    feat(service): check-battery

--- src/services.nix
+++ src/services.nix
@@ -1,16 +1,16 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
+{ config
+, pkgs
+, lib
+, ...
 }:
 with builtins; let
   std = pkgs.lib;
-in {
-  options = with lib; {};
-  imports = [];
+in
+{
+  options = with lib; { };
+  imports = [ ];
   config = {
-    # services.check-battery.enable = true;
+    services.check-battery.enable = true;
     services.kdeconnect.indicator = true;
     services.blueman-applet.enable = true;
   };
