1c4cb6a8882aca95ddce827dac504ab7d1d8b144
Ash Walker <ashurstwalker@gmail.com>
2022-10-19 15:06:14 -0400

    feat: scratchpad config

--- src/media/programs/im.nix
+++ src/media/programs/im.nix
@@ -1,18 +1,23 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
+{ config
+, pkgs
+, lib
+, ...
 }:
 with builtins; let
   std = pkgs.lib;
-in {
-  options = with lib; {};
-  imports = [];
+in
+{
+  options = with lib; { };
+  imports = [ ];
   config = {
     home.packages = with pkgs; [
       discord-canary
       element-desktop
+    ];
+    signal.desktop.wayland.compositor.scratchpads = [
+      { kb = "Shift+D"; criteria = { class = "discord"; }; resize = 93; startup = "discordcanary"; }
+      { kb = "Shift+M"; criteria = { class = "Element"; }; resize = 93; startup = "element-desktop"; }
+      { kb = "Shift+I"; criteria = { app_id = "scratch_irc"; }; resize = 83; startup = "kitty --class scratch_irc weechat"; }
     ];
   };
 }
