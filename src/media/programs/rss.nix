0822db656a31850724333f5588f1987862a8d9e6
Ash Walker <ashurstwalker@gmail.com>
2022-11-11 18:26:56 -0500

    feat: rss

---
+++ src/media/programs/rss.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+in {
+  options = with lib; {};
+  disabledModules = [];
+  imports = [];
+  config = {
+  	home.packages = with pkgs; [ rssguard ];
+    signal.desktop.wayland.compositor.scratchpads = [
+      {
+        kb = "Shift+R";
+        criteria = {app_id = "rssguard";};
+        resize = 83;
+        startup = "rssguard";
+      }
+    ];
+  };
+  meta = {};
+}
