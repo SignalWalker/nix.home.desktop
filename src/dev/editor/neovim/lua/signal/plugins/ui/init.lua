b78a346a6206c2e72bf168781a914cf83e269d40
Ash Walker <ashurstwalker@gmail.com>
2022-12-09 12:33:36 -0500

    fix(neovim): dapui function opts

--- src/dev/editor/neovim/lua/signal/plugins/ui/init.lua
+++ src/dev/editor/neovim/lua/signal/plugins/ui/init.lua
@@ -164,9 +164,9 @@ function M.dap_ui()
     dapui.setup{}
 
     local dap = require'dap'
-    dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
-    dap.listeners.after.event_terminated['dapui_config'] = function() dapui.close() end
-    dap.listeners.after.event_exited['dapui_config'] = function() dapui.close() end
+    dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open({}) end
+    dap.listeners.after.event_terminated['dapui_config'] = function() dapui.close({}) end
+    dap.listeners.after.event_exited['dapui_config'] = function() dapui.close({}) end
 end
 
 function M.nvimtree()
