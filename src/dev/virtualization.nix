a1a5ebad1913c2d01f681e4c42b5def746b85eb6
Ash Walker <ash@ashwalker.net>
2023-07-18 09:19:12 -0400

    feat: virtualization

---
+++ src/dev/virtualization.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  osConfig,
+  pkgs,
+  lib,
+  ...
+}:
+with builtins; let
+  std = pkgs.lib;
+  virt = config.signal.dev.virtualization;
+in {
+  options = with lib; {
+    signal.dev.virtualization = {
+      enable = (mkEnableOption "virtualization") // {default = osConfig.virtualisation.libvirt.enable or false;};
+    };
+  };
+  disabledModules = [];
+  imports = [];
+  config = lib.mkIf virt.enable {
+    # from https://nixos.wiki/wiki/Libvirt
+    xdg.configFile."libvirt/qemu.conf".text = ''
+      nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
+    '';
+    home.packages = [pkgs.virt-manager];
+  };
+  meta = {};
+}
