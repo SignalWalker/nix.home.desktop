08565d4b387164000d148a26297cdeb3c1eac775
Ash Walker <ashurstwalker@gmail.com>
2023-09-18 16:49:39 -0400

    ...

--- src/dev/lang/nix/templates.nix
+++ src/dev/lang/nix/templates.nix
@@ -5,44 +5,9 @@
   ...
 }: {
   config = lib.mkIf (config.signal.dev.lang.nix.enable) {
-    xdg.userDirs.templateFile."module.nix".text = ''
-      {
-        config,
-        pkgs,
-        lib,
-        ...
-      }:
-      with builtins; let
-        std = pkgs.lib;
-      in {
-        options = with lib; {};
-        disabledModules = [];
-        imports = [];
-        config = {};
-        meta = {};
-      }
-    '';
-    xdg.userDirs.templateFile."flake.nix".text = ''
-      {
-        description = "";
-        inputs = {
-          nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-          alejandra = {
-            url = "github:kamadorueda/alejandra";
-            inputs.nixpkgs.follows = "nixpkgs";
-          };
-        };
-        outputs = inputs @ {
-          self,
-          nixpkgs,
-          ...
-        }:
-          with builtins; let
-            std = nixpkgs.lib;
-          in {
-            formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
-          };
-      }
-    '';
+    xdg.userDirs.templateFile."nix/module.nix".source = ./templates/module.nix;
+    xdg.userDirs.templateFile."nix/flake.nix".source = ./templates/flake.nix;
+    xdg.userDirs.templateFile."nix/flake-rust.nix".source = ./templates/flake-rust.nix;
+    xdg.userDirs.templateFile."nix/flake-python.nix".source = ./templates/flake-python.nix;
   };
 }
---
+++ src/dev/lang/nix/templates/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    alejandra = {
+      url = "github:kamadorueda/alejandra";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }:
+    with builtins; let
+      std = nixpkgs.lib;
+    in {
+      formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
+    };
+}
