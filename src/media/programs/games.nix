a4493c531362cd1a2469ee4c8366ae26c85acbf0
Ash Walker <ashurstwalker@gmail.com>
2024-04-18 12:48:31 -0400

    ...

--- src/media/programs/games.nix
+++ src/media/programs/games.nix
@@ -8,7 +8,7 @@ with builtins; let
   std = pkgs.lib;
 in {
   options = with lib; {};
-  imports = lib.signal.fs.path.listFilePaths ./games;
+  imports = [];
   config = {
     home.packages = with pkgs; [
       dolphin-emu-beta
@@ -27,14 +27,8 @@ in {
           mupen64plus
         ];
       })
-      xdelta
-      # xivlauncher
-      lutris
-      cockatrice
-      xorg.xauth # for docker-mtgo
-      moonlight-qt
-      prismlauncher
       heroic
+      itch
     ];
     programs.mangohud = {
       enable = true;
@@ -44,25 +38,5 @@ in {
       settingsPerApplication = {
       };
     };
-    # home.packages = with pkgs; [
-    #   moonlight-qt
-    # ];
-    # systemd.user.services."sunshine" = {
-    #   Unit = {
-    #     Description = "self-hosted game stream host for Moonlight";
-    #     After = ["network-online.target"];
-    #     Wants = ["network-online.target"];
-    #     StartLimitIntervalSec = 500;
-    #     StartLimitBurst = 5;
-    #   };
-    #   Service = {
-    #     ExecStart = "${pkgs.sunshine}/bin/sunshine";
-    #     Restart = "on-failure";
-    #     RestartSec = "5s";
-    #   };
-    #   Install = {
-    #     WantedBy = ["graphical-session.target"];
-    #   };
-    # };
   };
 }
