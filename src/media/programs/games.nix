ef15c9d4d6aee90b33a272f87009900301dbad7c
Ash Walker <ashurstwalker@gmail.com>
2024-05-09 11:02:03 -0400

    ...

--- src/media/programs/games.nix
+++ src/media/programs/games.nix
@@ -32,8 +32,38 @@ in {
     ];
     programs.mangohud = {
       enable = true;
-      enableSessionWide = false;
+      enableSessionWide = true;
       settings = {
+        fps_limit = 60;
+        fps_limit_method = "early";
+        vsync = 0;
+
+        # # disable linear texture filtering
+        # retro = true;
+
+        time = true;
+        time_no_label = true;
+        time_format = "%H:%M";
+
+        ram = true;
+        vram = true;
+
+        gpu_name = true;
+        vulkan_driver = true;
+
+        wine = true;
+        winesync = true;
+
+        gamemode = true;
+
+        position = "bottom-right";
+        font_size = 12;
+        font_size_text = 12; # ???
+        hud_compact = true;
+        hud_no_margin = true;
+
+        toggle_hud = "Shift_R+F12";
+        toggle_hud_position = "Shift_R+F11";
       };
       settingsPerApplication = {
       };
