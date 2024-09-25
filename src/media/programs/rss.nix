3d6c0c4fb3c94169a6436945ccdd928bbbec7d66
Ash Walker <ashurstwalker@gmail.com>
2024-09-25 12:10:16 -0400

    for-sys: 2024-09-25T12:10-04:00

--- src/media/programs/rss.nix
+++ src/media/programs/rss.nix
@@ -11,15 +11,16 @@ in {
   disabledModules = [];
   imports = [];
   config = {
-    home.packages = with pkgs; [rssguard];
+    home.packages = with pkgs; [fluent-reader];
     desktop.scratchpads = {
       "Shift+R" = {
         criteria = {
-          app_id = "io.github.martinrotter.rssguard";
-          title = "^(\\[[0-9]*\\] )?RSS Guard [0-9]\\.[0-9]\\.[0-9]";
+          instance = "fluent-reader";
+          # app_id = "io.github.martinrotter.rssguard";
+          # title = "^(\\[[0-9]*\\] )?RSS Guard [0-9]\\.[0-9]\\.[0-9]";
         };
         resize = 93;
-        startup = "rssguard";
+        startup = "fluent-reader";
         systemdCat = true;
         automove = true;
         autostart = true;
@@ -28,3 +29,4 @@ in {
   };
   meta = {};
 }
+
