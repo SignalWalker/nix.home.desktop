5cb2c738afc9a10aad89fda3cc1baffc143a63f2
Ash Walker <ashurstwalker@gmail.com>
2025-03-28 08:01:38 -0400

    for-sys: 2025-03-28T08:01-04:00

--- src/wayland/compositor.nix
+++ src/wayland/compositor.nix
@@ -8,10 +8,8 @@ with builtins; let
   std = pkgs.lib;
   cfg = config.desktop.wayland.compositor;
 in {
-  options = with lib; {
-  };
+  options = with lib; {};
   imports = lib.signal.fs.path.listFilePaths ./compositor;
   config = {
   };
 }
-
